7be93cdb747ab2200ca7e0b2dd3a1031ece80f8b
shabrinaulia <shabrinaulia04@gmail.com>
2024-09-11 14:17:27 +0700

    tutorial 2 selesai

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://shabrina-aulia31-mentalhealthtracker.pbp.cs.ui.ac.id", "http://shabrina-aulia31-mentalhealthtracker.pbp.cs.ui.ac.id"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://shabrina-aulia31-mentalhealthtracker.pbp.cs.ui.ac.id", "https://shabrina-aulia31-mentalhealthtracker.pbp.cs.ui.ac.id"]
