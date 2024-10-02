3d0411787a320cd532d0d2cb260f8ef9a6d0993b
shabrinaulia <shabrinaulia04@gmail.com>
2024-10-02 13:46:02 +0700

    pws error

--- main/views.py
+++ main/views.py
@@ -15,6 +15,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
 
+
 @login_required(login_url='/login')
 def show_main(request):
 
